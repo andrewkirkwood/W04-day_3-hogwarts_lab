12b02a99edeb766229ddce5e6cf8815a9715726c
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2019-12-11 13:51:06 +0000

    display student page complete

--- students_controller.rb
+++ students_controller.rb
@@ -5,9 +5,14 @@ require_relative('models/house')
 also_reload('./models/*')
 
 # index
-
+get '/hogwarts' do
+  erb(:index)
+end
 # show
-
+get '/hogwarts/students' do
+  @students = Student.all()
+  erb(:students)
+end
 # new
 
 # create
