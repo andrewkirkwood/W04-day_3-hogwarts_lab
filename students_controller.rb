1e6211cf4eb2dbe3dcada179d4d15b0e80ad72ad
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2019-12-11 14:14:40 +0000

    create and save student complete

--- students_controller.rb
+++ students_controller.rb
@@ -4,19 +4,25 @@ require_relative('models/student')
 require_relative('models/house')
 also_reload('./models/*')
 
+# home
+get '/hogwarts/?' do
+  erb(:home)
+end
 # index
-get '/hogwarts' do
+get '/hogwarts/students/?' do
+  @students = Student.all()
   erb(:index)
 end
-# show
-get '/hogwarts/students' do
-  @students = Student.all()
-  erb(:students)
-end
 # new
-
+get '/hogwarts/students/new/?' do
+  erb(:new)
+end
 # create
-
+post '/hogwarts/students/?' do
+  @student = Student.new(params)
+  @student.save
+  erb(:create)
+end
 # edit
 
 # update
